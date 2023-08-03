ce629357863ae4970f5f235ab46bc761f7e7c1b3
Muhammad Muslim Abdul Jabbaar <83753102+MMuslimAbdulJ@users.noreply.github.com>
2023-08-03 23:11:37 +0700

    Update git-prompt.sh

--- git-prompt.sh
+++ git-prompt.sh
@@ -9,13 +9,10 @@ if test -f ~/.config/git/git-prompt.sh
 then
 	. ~/.config/git/git-prompt.sh
 else
-	# PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
 	PS1='\[\033]0;$PWD\007\]' # set window title
 	PS1="$PS1"'\n'                 # new line
 	PS1="$PS1"'\[\033[32m\]'       # change to green
 	PS1="$PS1"'\u@\h '             # user@host<space>
-	# PS1="$PS1"'\[\033[35m\]'       # change to purple
-	# PS1="$PS1"'$MSYSTEM '          # show MSYSTEM
 	PS1="$PS1"'\[\033[33m\]'       # change to brownish yellow
 	PS1="$PS1"'\w'                 # current working directory
 	if test -z "$WINELOADERNOEXEC"
